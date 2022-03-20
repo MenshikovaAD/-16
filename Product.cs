cd5c168a84beb433dcb5be9c66b5ab59fa1b1ec8
Анастасия Меньшикова <menschickova2011@mail.ru>
2022-03-20 15:03:38 +0300

    Добавьте файлы проекта.

---
+++ Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_16
+{
+    internal class Product
+    {
+        public int ProductCode { get; set; } 
+        public string ProductName { get; set; } 
+        public double ProductPrice { get; set; } 
+
+    }
+}
